abeb68045ae57e7bd51a2112c92d62865d9b5813
Julien Vallée <j.vallee@kayrros.com>
2019-07-26 08:46:21 +0200

    Show the number of active VPN

--- polybar/scripts/openvpn.sh
+++ polybar/scripts/openvpn.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-if [ "$(pgrep -x openvpn)" ]; then
-    echo "  VPN On "
+vpns=$(systemctl list-units | grep openvpn-client@)
+vpns_count=$(echo "$vpns" | wc -l)
+vpns_active=$(echo "$vpns" | grep active | wc -l)
+
+if [ $vpns_active -ne 0 ]; then
+    echo "  VPN On ($vpns_active/$vpns_count) "
 else
     echo "  No VPN "
 fi
